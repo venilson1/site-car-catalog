4717237438abc4b157d3050efb3585bc93c11f5b
venilson1 <venilson2009@hotmail.com>
2022-10-30 02:54:35 -0300

    feat: correcao design homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { Box, Container } from '@mui/system'
 import React, { useEffect, useState } from 'react'
 import MenuAppBar from '../components/MenuAppBar'
 import { getCars } from '../services/api';
+import FormatPrice from '../utils/FormatPrice';
 
 export default function HomePage() {
 
@@ -44,15 +45,10 @@ export default function HomePage() {
                                 />
                                 <CardContent>
                                     <Typography gutterBottom variant="h5" component="div">
-                                    {el.name}
+                                        {el.name} {el.brand} {el.model}
                                     </Typography>
-                                    <Typography gutterBottom variant="span" component="div">
-                                        {el.brand} {el.model}
-                                    </Typography>
-                                        <Typography variant="body2" color="text.secondary">
-                                        <Typography variant="h4" component="span" color="primary">{el.price}</Typography>
-                                        <Typography variant="h6" component="span">R$</Typography>
-                                    </Typography>
+
+                                        <Typography >{FormatPrice(el.price)}</Typography>
                                 </CardContent>
                             </Card>
                         )
